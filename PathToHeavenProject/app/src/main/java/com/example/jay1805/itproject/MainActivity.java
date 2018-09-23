e70a7742a5583b335ac7e5bdf537d8530b6b1b2e
Hasitha Dias <30955334+hdias5000@users.noreply.github.com>
2018-09-23 15:20:43 +1000

    starting menu

--- PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
+++ PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.jay1805.itproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mapButton;
+    private Button chatsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapButton = findViewById(R.id.B_map);
+        chatsButton = findViewById(R.id.B_chat);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 }
