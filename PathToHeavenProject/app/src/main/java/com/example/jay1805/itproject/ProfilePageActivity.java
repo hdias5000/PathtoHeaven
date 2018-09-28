54767ccc5318fab4123de342a70458b876274c05
asodhi19 <asodhi@student.unimelb.edu.au>
2018-09-28 19:06:18 +1000

    Added profile form with calender in progress

--- PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/ProfilePageActivity.java
+++ PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/ProfilePageActivity.java
@@ -1,5 +1,6 @@
 package com.example.jay1805.itproject;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,7 +20,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class ProfilePageActivity extends AppCompatActivity {
@@ -26,7 +31,8 @@ public class ProfilePageActivity extends AppCompatActivity {
     private EditText HomeAddress;
     private TextView nameOfUser;
     private Button saveDetails;
-
+    private EditText dateEditText;
+    private Calendar myCalendar;
     private String name;
 
     private DatabaseReference userDB;
@@ -39,6 +45,37 @@ public class ProfilePageActivity extends AppCompatActivity {
         HomeAddress = (EditText) findViewById(R.id.home_address_editText);
         nameOfUser = (TextView) findViewById(R.id.theActualName);
         saveDetails = (Button) findViewById(R.id.save_button);
+        dateEditText  = (EditText) findViewById(R.id.dob_editText);
+        myCalendar =  Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getApplicationContext(),
+                        date,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+            }
+        });
+
+
+
 
         userDB = FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
@@ -89,5 +126,12 @@ public class ProfilePageActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dateEditText.setText(sdf.format(myCalendar.getTime()));
     }
 }
