cc3464a1836ac181b038601cc0d3764662af68ab
Jay Parikh <jparikh@student.unimelb.edu.au>
2018-09-29 21:05:21 +1000

    Made name appear when chat is created and also made changes in the menu

--- PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
+++ PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
@@ -6,10 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button mapButton;
     private Button chatsButton;
+    private Button findUserButton;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         mapButton = findViewById(R.id.B_map);
         chatsButton = findViewById(R.id.B_chat);
+        findUserButton = findViewById(R.id.findUser);
+        logoutButton = findViewById(R.id.logout_btn);
+
+
+        findUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(getApplicationContext(), FindUserActivity.class), 1);
+            }
+        });
 
         mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +46,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), ChatMainPageActivity.class));
             }
         });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                // make sure the user is who he says he is
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
     }
 
 }
