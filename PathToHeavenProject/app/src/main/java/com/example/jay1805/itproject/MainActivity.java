2fbe89e06e1a8f769a186df0cad5794efb23b5ed
Hasitha Dias <30955334+hdias5000@users.noreply.github.com>
2018-10-02 14:22:19 +1000

    updated main activity

--- PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
+++ PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.jay1805.itproject;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,12 +11,6 @@ public class MainActivity extends AppCompatActivity {
     private Button mapButton;
     private Button chatsButton;
 
-    Intent mServiceIntent;
-    private SensorService mSensorService;
-    Context ctx;
-    public Context getCtx() {
-        return ctx;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,35 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        ctx = this;
-//        mSensorService = new SensorService(getCtx());
-//        mServiceIntent = new Intent(getCtx(), mSensorService.getClass());
-//        if (!isMyServiceRunning(mSensorService.getClass())) {
-//            startService(mServiceIntent);
-//        }
     }
 
-
-    //////////////////////////////////////////////////////////////////////////////////////SERVICE
-    private boolean isMyServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                Log.i ("isMyServiceRunning?", true+"");
-                return true;
-            }
-        }
-        Log.i ("isMyServiceRunning?", false+"");
-        return false;
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        stopService(mServiceIntent);
-        Log.i("MAINACT", "onDestroy!");
-        super.onDestroy();
-
-    }
 
 }
