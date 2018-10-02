68dbcfab286b52ed05f747b7a4dfe0aff067e099
Jay Parikh <jparikh@student.unimelb.edu.au>
2018-10-02 22:16:29 +1000

    Basic changes

---
+++ PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MyProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.jay1805.itproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
