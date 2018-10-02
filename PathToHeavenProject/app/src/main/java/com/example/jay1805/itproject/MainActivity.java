a2acb2f5c60790a03fff5e1ecde19342825d408d
Jay Parikh <jparikh@student.unimelb.edu.au>
2018-10-02 12:37:30 +1000

    Minor changes

--- PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
+++ PathToHeavenProject/app/src/main/java/com/example/jay1805/itproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
